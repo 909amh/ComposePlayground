e0488595c72ad9fcd4e193d1e4721a241f276e34
909amh <909amh@gmail.com>
2025-03-01 12:00:11 -0600

    TimeDial now smoothly transitions scale

--- TimeDial/app/src/main/java/com/higgins/timedial/MainActivity.kt
+++ TimeDial/app/src/main/java/com/higgins/timedial/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.higgins.timedial
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,20 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.higgins.timedial.ui.theme.DefaultTheme
 import com.higgins.timedial.ui.timeDial.SingleDial
-import com.higgins.timedial.ui.timeDial.SingleDial2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     contentAlignment = Alignment.Center
                 ) {
                     Row {
-                        SingleDial2(
+                        SingleDial(
                             values = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"),
                             startIndex = 0
                         )
--- TimeDial/app/src/main/java/com/higgins/timedial/ui/timeDial/SingleDial.kt
+++ TimeDial/app/src/main/java/com/higgins/timedial/ui/timeDial/SingleDial.kt
@@ -1,35 +1,24 @@
 package com.higgins.timedial.ui.timeDial
 
-import android.nfc.Tag
 import android.util.Log
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.animate
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
@@ -39,119 +28,133 @@ import com.higgins.timedial.dpToPx
 import com.higgins.timedial.pxToDp
 import com.higgins.timedial.ui.theme.DefaultTheme
 import com.higgins.timedial.wrap
-import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
+private const val NUMBER_OF_CELLS = 9
 private const val CENTER_INDEX = 4
-private const val VISIBLE_ITEM_COUNT = 9
-private const val Tag = "SingleDial"
+private const val TAG = "SingleDial2"
 @Composable
 fun SingleDial(
     modifier: Modifier = Modifier,
     startIndex: Int = 0,
     values: List<String>
 ) {
+    // Environment
     val density = LocalDensity.current
-    val scope = rememberCoroutineScope()
 
-    var firstIndex by remember { mutableIntStateOf(startIndex) }
-    var dragOffset by remember { mutableFloatStateOf(0f) }
+    // The height of a single dial cell
+    val cellHeight = 36.dp.dpToPx(density)
 
-    val animatedOffset = remember { Animatable(0f) }
-    val coercedAnimatedOffset = animatedOffset.value % 24.dp.dpToPx(density)
+    // The height of the container for the dial cells
+    val columnHeight = (cellHeight * NUMBER_OF_CELLS).roundToInt().pxToDp(density)
+    // Current First Index
+    var currentStartIndex by remember { mutableIntStateOf(startIndex) }
 
-    val itemHeightPx = 24.dp.dpToPx(density)
-    LaunchedEffect(coercedAnimatedOffset) {
-        val threshold = itemHeightPx / 2
-        if (coercedAnimatedOffset > threshold) {
-            val newIndex = firstIndex - 1
-            firstIndex = if (newIndex >= 0) { (firstIndex - 1) % values.size }
-            else { values.lastIndex % values.size }
-
-            animatedOffset.snapTo(animatedOffset.value - itemHeightPx)
-
-        } else if (coercedAnimatedOffset < -threshold) {
-            firstIndex = (firstIndex + 1 + values.size) % values.size
-            animatedOffset.snapTo(animatedOffset.value + itemHeightPx)
+    // The Y Offset from the drag gesture
+    var dragOffsetY by remember { mutableFloatStateOf(0f) }
+    val coercedDragOffsetY = dragOffsetY % cellHeight
+    val draggableState = rememberDraggableState { delta ->
+        dragOffsetY += delta
+        if (dragOffsetY >= cellHeight) {
+            val newIndex = currentStartIndex - 1
+            currentStartIndex = if (newIndex < 0) {
+                values.lastIndex
+            } else {
+                newIndex
+            }
+            dragOffsetY = 0f
+        } else if (dragOffsetY <= -cellHeight) {
+            val newIndex = currentStartIndex + 1
+            currentStartIndex = if (newIndex >= values.size) 0 else newIndex
+            dragOffsetY = 0f
         }
+        Log.d(TAG, "Drag Offset: $dragOffsetY")
+        Log.d(TAG, "Cell Height: $cellHeight")
     }
-
-    Column(
-        modifier = Modifier
-            .height((itemHeightPx * 9).toInt().pxToDp(density))
-            .draggable(
-                orientation = Orientation.Vertical,
-                state = rememberDraggableState { deltaY ->
-                    scope.launch {
-                        animatedOffset.snapTo(animatedOffset.value + deltaY)
-                    }
-                }
-            ),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Box(
-            modifier = modifier
-                .offset {
-                    IntOffset(
-                        x = 0,
-                        y = coercedAnimatedOffset.roundToInt()
-                    )
-                },
-        ) {
-            val visibleValues = values.wrap(firstIndex, 9)
-            visibleValues.forEachIndexed { index, value ->
-                // Distance from the center
-                val relativeIndex = index - CENTER_INDEX
-                val yOffset = (relativeIndex * 24.dp.dpToPx(density))
-
-                var fontSize = 0.sp
-                when(index) {
-                    0 -> {
-                        fontSize = 6.sp
-                    }
-                    1 -> {
-                        fontSize = 12.sp
-                    }
-                    2 -> {
-                        fontSize = 17.sp
-                    }
-                    3 -> {
-                        fontSize = 21.sp
-                    }
-                    CENTER_INDEX -> {
-                        fontSize = 26.sp
-                    }
-                    5 -> {
-                        fontSize = 21.sp
-                    }
-                    6 -> {
-                        fontSize = 17.sp
-                    }
-                    7 -> {
-                        fontSize = 12.sp
-                    }
-                    8 -> {
-                        fontSize = 6.sp
-                    }
-                    else -> {
-                        fontSize = 0.sp
-                    }
-                }
-
-                DialSlot(
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .offset {
-                            IntOffset(
-                                x = 0,
-                                y = yOffset.toInt()
-                            )
-                        },
-                    data = value,
-                    fontSize = fontSize
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .height(columnHeight)
+            .width(56.dp)
+            .offset {
+                IntOffset(
+                    x = 0,
+                    y = coercedDragOffsetY.roundToInt()
                 )
             }
+            .draggable(
+                orientation = Orientation.Vertical,
+                state = draggableState
+            )
+    ) {
+        val visibleValues = values.wrap(currentStartIndex, 9)
+
+        visibleValues.forEachIndexed { index, value ->
+            val distanceFromCenter = index - CENTER_INDEX
+
+            val scale = calculateScale(
+                distanceFromCenter = distanceFromCenter,
+                dragOffset = dragOffsetY,
+                cellHeight = cellHeight
+            )
+
+            DialSlot(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .offset {
+                        IntOffset(
+                            x = 0,
+                            y = (distanceFromCenter * cellHeight.roundToInt())
+                        )
+                    }
+                    .scale(scale),
+                data = value,
+                fontSize = 14.sp
+            )
         }
+    }
+}
+
+/**
+ * Calculates the scale of a cell based on it's index and the current progress of the dragOffset.
+ */
+private fun calculateScale(
+    distanceFromCenter: Int,
+    dragOffset: Float,
+    cellHeight: Float
+): Float {
+    val baseScale = calculateBaseScale(distanceFromCenter)
+    val dragProgress = (dragOffset.absoluteValue / cellHeight)
+
+    val finalScale = when {
+        // Dragging Down
+        dragOffset > 0f -> {
+            val nextIndex = distanceFromCenter + 1
+            val nextBaseScale = calculateBaseScale(nextIndex)
+            baseScale + (nextBaseScale - baseScale) * dragProgress
+        }
+        // Dragging Up
+        dragOffset < 0f -> {
+            val nextIndex = distanceFromCenter - 1
+            val nextBaseScale = calculateBaseScale(nextIndex)
+            baseScale + (nextBaseScale - baseScale) * dragProgress
+        }
+        else -> baseScale
+    }
+
+    return finalScale
+}
+
+private fun calculateBaseScale(
+    distanceFromCenter: Int
+): Float {
+    return when (distanceFromCenter.absoluteValue) {
+        0 -> 3f
+        1 -> 2.5f
+        2 -> 2f
+        3 -> 1f
+        else -> 0.5f
     }
 }
 
@@ -160,7 +163,8 @@ fun SingleDial(
 private fun Preview() {
     DefaultTheme {
         SingleDial(
-            values = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+            startIndex = 0,
+            values = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
         )
     }
 }
